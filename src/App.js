962a9fdf288978efcab3849c00929193d15c42ae
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-06-30 00:22:43 +0530

    mouse

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import HookCounter1 from './components/HookCounter1';
+import HookMouse from './components/HookMouse';
 
 function App() {
   return (
     <div className="App">
       <HookCounter1 />
+      <HookMouse />
     </div>
   );
 }
