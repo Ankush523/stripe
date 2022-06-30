9f0a23dfec21374851ac7b4bf765f4465691866a
ankush.ad050203@gmail.com <ankush.ad050203@gmail.com>
2022-06-30 17:52:24 +0530

    sample

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import HookCounter1 from './components/HookCounter1';
 import HookMouse from './components/HookMouse';
+import IntervalHookCounter from './components/IntervalHookCounter';
 import MouseContainer from './components/MouseContainer';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <HookCounter1 />
       <HookMouse />
       <MouseContainer />
+      <IntervalHookCounter/>
     </div>
   );
 }
